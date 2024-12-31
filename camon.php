62bbba0e65610b1d1aa3adcfc11889e6f2e9004c
philehieu2007 <acclqphilehieu90@gmail.com>
2024-12-31 16:49:53 +0700

    Update camon.php

--- camon.php
+++ camon.php
@@ -1,6 +1,6 @@
 ﻿<html>
 <head>
-<title>TỚ ♥ CẬU - CHỈ VẬY THÔI</title>
+<title>ANH ♥ EM - CHỈ VẬY THÔI</title>
 <link rel="shortcut icon" href="images/14.ico" type="image/png" />
 <link rel="stylesheet" href="css/reset.css" />
     <link rel="stylesheet" href="css/style.css" />
@@ -14,19 +14,18 @@
 <body>
 <article id="cloud">
 <!--*************EDIT***********-->
-<p><font color="#d64f1e">Tao</font> <font color="#c41818">♥</font> <font color="#db3376">Mầy</font> - <font color="#fff">Nhìu VL</font></p>
+<p><font color="#d64f1e">ANH</font> <font color="#c41818">♥</font> <font color="#db3376">VỢ</font> - <font color="#fff">Nhìu VCL</font></p>
 </article>
 <footer id="city"></footer>
 <div id="dapxe"><center><embed width="500" height="500"  wmode="transparent" menu="false" quality="high" type="application/x-shockwave-flash" src="images/dapxe.swf" pluginspage="http://www.macromedia.com/go/getflashplayer"/></center></div>
-<div id="duong"><p> Liên hệ để được hổ trợ: 
-<font color="#dd8900"><b><a href="https://www.facebook.com/PhungQuocDinh" target="_blank" title="Bấm để được hỗ trợ"> Facebook</a></b></br></br>
+
 <!--*************EDIT***********-->
-Chém gió các kiểu ở đây</br>
-...............................</br>
-.................................</br>
-..................................</p></div>
+Anh yêu em nhiều nhất thế giới này</br>
+Sau này em đã hứa rùi chỉ được cưới tớ thui nhaa</br>
+Chúng ta sẽ có 1 đám cưới thật đẹp</br>
+Ta sẽ sống hạnh phúc với nhau, và cả con chúng mik nữaaa</p></div>
 <!--***** ĐỔI NHẠC TẠI ĐÂY ********-->
-<iframe src="http://www.nhaccuatui.com/mh/background/HCuL0jtcLErS" width="1" height="1" frameborder="0" allowfullscreen></iframe>>
+<iframe src="https://www.nhaccuatui.com/mh/background/C3Xrj0oZUhTJ" width="1" height="1" frameborder="0" allowfullscreen></iframe>>
 </body>
 <<style>
 	body{
